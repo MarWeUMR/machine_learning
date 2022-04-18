b5f38fe9de8f7d98486656c69baedd6d0b79ee16
marweumr <marcus.wallau@googlemail.com>
2022-04-18 09:41:13 +0200

    updates

--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,82 @@
 extern crate xgboost;
+use ndarray::prelude::*;
 
-use labello::*;
+use xgboost::DMatrix;
 
-use xgboost::{parameters, Booster, DMatrix};
+use pyo3::{
+    prelude::*,
+    types::{IntoPyDict, PyModule},
+};
 
-use polars::df;
 use polars::prelude::*;
 
 fn main() {
-    println!("Hello, world!");
+    // use python to preprocess data
+    let gil = pyo3::Python::acquire_gil();
+    let py = gil.python();
+    let _py_mod =
+        PyModule::from_code(py, include_str!("test.py"), "filename.py", "modulename").unwrap();
 
-    let x: DataFrame = CsvReader::from_path("datasets/urban/train_data.csv")
+    // read preprocessed data to rust
+    let x_train_frame: DataFrame = CsvReader::from_path("datasets/urban/train_data_enc.csv")
         .unwrap()
         .infer_schema(None)
         .has_header(true)
         .finish()
         .unwrap();
 
-    let y = x.column("class").unwrap().to_owned();
+    let x_test_frame: DataFrame = CsvReader::from_path("datasets/urban/test_data_enc.csv")
+        .unwrap()
+        .infer_schema(None)
+        .has_header(true)
+        .finish()
+        .unwrap();
 
-    let z: Vec<_> = y.utf8().into_iter().collect();
+    let y_train_frame =
+        DataFrame::new(vec![x_train_frame.column("class_enc").unwrap().clone()]).unwrap();
+    let y_train_array = y_train_frame.to_ndarray::<Float32Type>().unwrap();
+    x_train_frame.drop("class_enc").unwrap();
 
-    for (i, elem) in y.utf8().into_iter().enumerate() {
-        println!("i: {:?}, {:?}", i, elem);
-    }
+    let y_test_frame =
+        DataFrame::new(vec![x_test_frame.column("class_enc").unwrap().clone()]).unwrap();
+    let y_test_array = y_test_frame.to_ndarray::<Float32Type>().unwrap();
+    x_test_frame.drop("class_enc").unwrap();
 
-    // println!("{:?}", z);
-    //
-    // let enctype = EncoderType::Ordinal;
-    // let config = Config{
-    //     mapping_function:None,
-    //     max_nclasses: Some(y.n_unique().unwrap() as u64),
-    // };
-    // let mut enc: Encoder<String> = Encoder::new(Some(enctype));
-    //
-    // enc.fit(&z, &config);
-    //
-    //
-    //
-    //
-    //
-    //
-    //
-    //
-    //
-    //
-    //
-    //
-    //
-    //
-    //
-    //
-    //
-    //
+    let x_train_array: Array<f32, _> = x_train_frame.to_ndarray::<Float32Type>().unwrap();
+    let x_test_array: Array<f32, _> = x_test_frame.to_ndarray::<Float32Type>().unwrap();
 
-    println!("{:?}", x);
+    let num_rows_train = &x_train_array.len();
+    let num_rows_test = &x_test_array.len();
+
+    // println!("{:?}", x_train_array.shape()[0]);
+
+    let train_shape = x_train_array.raw_dim();
+    let test_shape = x_test_array.shape();
+
+
+    println!("{:?}", train_shape);
+
+    let mut dtrain = DMatrix::from_dense(
+        x_train_array
+            .into_shape(train_shape[0] * train_shape[1])
+            .unwrap()
+            .as_slice()
+            .unwrap(),
+        *num_rows_train,
+    )
+    .unwrap();
+    
+    let mut dtest = DMatrix::from_dense(
+        x_test_array
+            .into_shape(test_shape[0] * test_shape[1])
+            .unwrap()
+            .as_slice()
+            .unwrap(),
+        *num_rows_test,
+    )
+    .unwrap();
+
+    // dtrain
+    //     .set_labels(y_train_array.as_slice().unwrap())
+    //     .unwrap();
 }
--- src/test.py
+++ src/test.py
@@ -1,2 +1,19 @@
-from sklearn import preprocessing
-import numpy as np
+import pandas as pd
+
+from sklearn.preprocessing import LabelEncoder
+
+df_train = pd.read_csv('datasets/urban/train_data.csv')
+df_test = pd.read_csv('datasets/urban/test_data.csv')
+
+label_encoder = LabelEncoder()
+df_train["class_enc"] = label_encoder.fit_transform(df_train["class"])
+df_train.drop(["class"], axis=1, inplace=True)
+df_train.to_csv('datasets/urban/train_data_enc.csv')
+
+label_encoder = LabelEncoder()
+df_test["class_enc"] = label_encoder.fit_transform(df_test["class"])
+df_test.drop(["class"], axis=1, inplace=True)
+df_test.to_csv('datasets/urban/test_data_enc.csv')
+
+
+print("python done")
